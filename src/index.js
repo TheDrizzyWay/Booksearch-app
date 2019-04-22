1e5615f7f123b3d1b0c44c858ddfbac733226725
Christopher Iliyas <christopher.iliyas@andela.com>
2019-04-22 18:51:53 +0100

    include html loaders

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 class App extends Component {
     render () {
-        return <h1>Welcome to the Drizzy bookfinder</h1>;
+        return <h1>Welcome to the Drizzy Bookfinder</h1>;
     }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   devtool: 'inline-source-map',
   entry: './src/index.js',
   output: {
-    path: __dirname + '/dist',
-    publicPath: '/',
+    path: __dirname + 'dist/',
+    publicPath: '/dist',
     filename: 'bundle.js'
   },
   devServer: {
-    contentBase: './dist'
+    contentBase: './dist',
+    port: 3000,
+    publicPath: "http://localhost:3000/dist/",
   },
   module: {
     rules: [
@@ -15,7 +19,23 @@ module.exports = {
       test: /\.(js|jsx)$/,
       exclude: /node_modules/,
       use: ['babel-loader', 'eslint-loader']
-    }
+    },
+     {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    },
+    {
+      test: /\.(png|jpe?g|gif|svg|ico)$/i,
+      use: [{
+          loader: 'url-loader',
+          options: { limit: 8192 }
+        }]
+      }
     ]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ]
 };
