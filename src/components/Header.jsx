d8a048a0a2f2a60fd9766af9326153f9b2c02363
Christopher Iliyas <christopher.iliyas@andela.com>
2019-04-26 16:26:44 +0100

    fix header style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,10 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
 import { fade } from '@material-ui/core/styles/colorManipulator';
 import { withStyles } from '@material-ui/core/styles';
-import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 
 const styles = theme => ({
@@ -16,10 +14,6 @@ const styles = theme => ({
     },
     grow: {
         flexGrow: 1
-    },
-    menuButton: {
-        marginLeft: -12,
-        marginRight: 20
     },
     title: {
         display: 'flex',
@@ -102,11 +96,8 @@ class SearchAppBar extends Component {
           <div className={classes.root}>
               <AppBar position="static">
                   <Toolbar>
-                      <IconButton className={classes.menuButton} color="inherit" aria-label="Open drawer">
-                          <MenuIcon />
-                      </IconButton>
                       <Typography className={classes.title} variant="h6" color="inherit">
-            Welcome to the Drizzy Bookfinder
+            Welcome to the Drizzy Bookfinder App
                       </Typography>
                       <div className={classes.grow} />
                       <div className={classes.search}>
