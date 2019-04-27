15d5ee09ddd254cc7799dc51944a1a40da05a4db
Christopher Iliyas <christopher.iliyas@andela.com>
2019-04-27 21:03:45 +0100

    add booklist component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import SearchAppBar from './components/Header';
+import BookList from './components/BookList';
 import './App.css';
 
 const searchUrl = 'https://www.googleapis.com/books/v1/volumes';
 
 class App extends Component {
   state = {
-      books: []
+      books: [],
+      searchTerm: ''
   }
 
   searchBook = (userinput) => {
       axios.get(`${searchUrl}?q=${userinput}`)
-          .then(res => console.log(res.data))
+          .then(res => this.setState({
+              books: res.data.items,
+              searchTerm: userinput
+          }))
           .catch(err => console.log(err));
   };
 
   render () {
+      const { books, searchTerm } = this.state;
       return (
           <>
           <SearchAppBar searchBook={this.searchBook}/>
+          <BookList books={books} searchTerm={searchTerm} />
           </>
       );
   }
---
+++ src/components/BookList.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class BookList extends Component {
+    render () {
+        const { props: { books, searchTerm } } = this;
+        if (!searchTerm.length) return <h1>Enter your book name or author</h1>;
+        return (
+            books.length ? (
+                books.map(book => <h1>{book.volumeInfo.title}</h1>)
+            ) : (
+                <h1> No results found</h1>
+            )
+        );
+    }
+}
+
+BookList.propTypes = {
+    books: PropTypes.array,
+    searchTerm: PropTypes.string
+};
+
+export default BookList;
